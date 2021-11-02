fcc95f0efa26bb5e33c931d5595fefb110ec52e4
jcpcabanada <jcpcabanada@gmail.com>
2021-11-01 19:27:00 -0700

    first

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@
   Look at each file to see what props need to be passed!
 */
 
-// Import the state hook
-import React, {useState} from 'react';
-// Import the Posts (plural!) and SearchBar components, since they are used inside App component
-import Posts from './components/Posts';
-import SearchBar from "./components/SearchBar/SearchBar";
-// Import the dummyData
-import dummyData from "./dummy-data";
+//***  Import the state hook
+import React, { useState } from 'react';
+//***  Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar'
+//*** Import the dummyData
+import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
-    // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+    //*** Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
     const [posts, setPosts] = useState(dummyData);
+    const [search, setSearch] = useState('');
     // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
 
     // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-    const [search, setSearch] = useState('');
+
 
     const likePost = postId => {
+
         /*
-          This function serves the purpose of increasing the number of likes by one, of the post with a given id.
+          ***This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
           The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
           This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
@@ -34,17 +36,18 @@ const App = () => {
             - otherwise just return the post object unchanged.
          */
         const funPosts = posts.map(post => {
-            return post.id === postId ? { ...post, likes : post.likes + 1 }
+            return post.id === postId ? {...post, likes : post.likes + 1} : post
+
         })
         setPosts(funPosts);
     };
 
     return (
         <div className='App'>
-            {/* Add SearchBar and Posts here to render them */}
+            {/* ***Add SearchBar and Posts here to render them */}
+            {/* ***Check the implementation of each component, to see what props they require, if any! */}
             <SearchBar />
             <Posts likePost={likePost} posts={posts} />
-            {/* Check the implementation of each component, to see what props they require, if any! */}
         </div>
     );
 };
